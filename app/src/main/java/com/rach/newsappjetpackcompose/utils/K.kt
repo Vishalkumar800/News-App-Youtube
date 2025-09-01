9358350c11336926e6d9e44ffa166b790c05a189
Vishal <156772524+Vishalkumar800@users.noreply.github.com>
2025-09-01 22:27:59 +0530

    Update K.kt

--- app/src/main/java/com/rach/newsappjetpackcompose/utils/K.kt
+++ app/src/main/java/com/rach/newsappjetpackcompose/utils/K.kt
@@ -1,6 +1,6 @@
 package com.rach.newsappjetpackcompose.utils
 
 object K {
-    const val API_KEY = "f963fa84cdf54b448720f5643b55cd0c"
+    const val API_KEY = ""
     const val BASE_URL = "https://newsapi.org/"
 }
